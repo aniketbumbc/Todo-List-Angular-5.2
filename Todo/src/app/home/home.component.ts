162a9214dfac80c65b5bee609979e0afb5bc01f3
aniket umbc <aniket.umbc@gmail.com>
2018-03-01 00:09:20 +0530

    Added Property Binding

--- Todo/src/app/home/home.component.ts
+++ Todo/src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  homeTitle = "Welcome to the homepage";
   constructor() { }
 
   ngOnInit() {
