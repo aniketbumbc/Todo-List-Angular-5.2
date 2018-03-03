95aea6446267dbf09aebf82afd5261b832fe4e2f
aniket umbc <aniket.umbc@gmail.com>
2018-03-03 17:56:39 +0530

    Implement Route Prams

--- Todo/src/app/app-routing.module.ts
+++ Todo/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {DirectoryComponent} from './directory/directory.component';
 import {HomeComponent} from './home/home.component';
 
 const routes:Routes =[
-  { path : 'Directory' , component: DirectoryComponent},
+  { path : 'directory' , component: DirectoryComponent},
   { path : 'Home' , component: HomeComponent},
   { path : '' , component: HomeComponent}
 ];
--- Todo/src/app/directory/directory.component.ts
+++ Todo/src/app/directory/directory.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-directory',
   templateUrl: './directory.component.html',
   styleUrls: ['./directory.component.css']
 })
 export class DirectoryComponent implements OnInit {
-
-  constructor() { }
+list:string;
+  constructor(private route:ActivatedRoute) {
+this.list=route.snapshot.params['list'];
+   }
 
   ngOnInit() {
   }
